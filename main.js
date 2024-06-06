a8211a1b1505367142f1288c673b95df4bd5f53e
Bautista Piaggio <bauti.piaggio@gmail.com>
2024-06-06 18:12:49 -0300

    librerias y carga de datos agregados

--- main.js
+++ main.js
@@ -40,13 +40,7 @@ class Alumno{
         promedio >= 6 && (this.aprobar = true);
         promedio >= 7 && (this.promocionar = true);    
 
-        /*      if(promedio>=6){
-            this.aprobar=true;        
-
-        }
-        if(promedio>=7){
-            this.promocionar=true; 
-        } */
+    
 
     };
 }
@@ -60,92 +54,6 @@ const Alumno06 = new Alumno("Matias","Taranto", 22, 3, "Licenciatura en Biologí
 
 const arregloDeAlumnos = [Alumno01, Alumno02, Alumno03, Alumno04, Alumno05, Alumno06];
 
-/* 
-let opcion;
-
-do {
-    opcion = parseInt(prompt("Ingrese opción:\n1-Listar alumnos\n2-Cargar notas\n3-Actualizar estado de aprobación\n4-Filtrar alumnos\n5-Salir"));
-
-    switch (opcion) {
-        case 1:
-            console.log(arregloDeAlumnos);
-            break;
-        case 2:
-            arregloDeAlumnos.forEach(alumno => alumno.cargarNotas());
-            break;
-        case 3:
-            arregloDeAlumnos.forEach(alumno => alumno.aprobado());
-            break;
-        case 4:
-            let opcion4 = parseInt(prompt("Seleccione filtro:\n1-Ingeniería\n2-Tecnicatura\n3-Licenciatura"));
-            if (opcion4 === 1) {
-                const alumnosDeIngeneria = arregloDeAlumnos.filter((el) => el.carrera.includes("Ingeniería"));
-                console.log(alumnosDeIngeneria);
-            } else if (opcion4 === 2) {
-                const alumnosDeTecnicatura = arregloDeAlumnos.filter((el) => el.carrera.includes("Tecnicatura"));
-                console.log(alumnosDeTecnicatura);
-            } else if (opcion4 === 3) {
-                const alumnosDeLicenciatura = arregloDeAlumnos.filter((el) => el.carrera.includes("Licenciatura"));
-                console.log(alumnosDeLicenciatura);
-            } else {
-                console.log("Ingrese una opción válida");
-            }
-            break;
-        case 5:
-            console.log("Saliendo del programa...");
-            break;
-        default:
-            console.log("Ingrese una opción válida");
-            break;
-    }
-} while (opcion !== 5); */
-
-
-
-
-/* //convertimos el alumno a json
-const alumno01json = JSON.stringify(Alumno01);
-console.log(alumno01json);
-//guardamos el json a local storage
-localStorage.setItem("keyAlumno01", alumno01json);
-//parseamos de vuelta a objeto
-const alumno01parse = JSON.parse(alumno01json)
-console.log(alumno01parse)
-console.log(alumno01parse.edad)
-
-
-const alumnos = JSON.stringify(arregloDeAlumnos);
-localStorage.setItem("Alumnos-key", alumnos) */
-
-
-
-
-/*
-
-<table>
-    <tr>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>edad</th>
-        <th>Año de cursado</th>
-        <th>carrera</th>
-        <th>Nota (parcial 1, 2 y 3)</th>
-        <th>Aprobado</th>
-        <th>Promocionado</th>
-    </tr>
-
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-</table>    
-*/
 function renderizarAlumnos(){
     tabla.innerHTML="";
     let tabladom = document.getElementById("tabla")
@@ -167,7 +75,7 @@ function renderizarAlumnos(){
         <td>${alumno.apellido}</td>
         <td>${alumno.edad}</td>
         <td>${alumno.anioDeCursado}</td>
-        <td>${alumno.carrera}</td>
+        <td class="clickcarrera">${alumno.carrera}</td>
         <td id="notas123">${alumno.notaParcial12y3}</td>
         <td id="apr">${alumno.aprobar}</td>
         <td id="prom">${alumno.promocionar}</td>
@@ -180,58 +88,76 @@ renderizarAlumnos();
 function obtenerNotasGuardadas(){
     arregloDeAlumnos.forEach(alumno =>{
         const notasGuardadas = localStorage.getItem(`${alumno.nombre}_${alumno.apellido}_notas`);
-  /*   if (notasGuardadas) {
-        alumno.notaParcial12y3 = JSON.parse(notasGuardadas);
-    } */
+
     alumno.notaParcial12y3 = notasGuardadas ? JSON.parse(notasGuardadas) : alumno.notaParcial12y3;
     });
 }
 
-/* let botondom = document.getElementById("botones");
-document.createElement(<button>olo</button>); */
 let botondom = document.createElement("button");
 botondom.innerHTML="Cargar Notas";
-document.body.append(botondom);
+botones.append(botondom);
 
 botondom.addEventListener("click", eventoBotondom);
 function eventoBotondom() {
     arregloDeAlumnos.forEach(alumno => alumno.cargarNotas());
     renderizarAlumnos();
-    // alert("Notas cargadas a local storage");
+   
 }
 
 let botondom2 = document.createElement("button");
 botondom2.innerHTML="Actualizar Aprobados";
-document.body.append(botondom2);
+botones.append(botondom2);
 
 botondom2.addEventListener("click", eventoBotondom2);
 function eventoBotondom2() {
     arregloDeAlumnos.forEach(alumno => alumno.aprobado());
     renderizarAlumnos();
+
+    Toastify({
+        text: "Estado de aprobación actualizado",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", 
+        position: "left", 
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to right, #004225  , #37383f)",
+        },
+        onClick: function(){}
+    }).showToast();
 }
 
-/* let botondom3 = document.createElement("button");
-botondom3.innerHTML="log";
-document.body.append(botondom3);
-
-botondom3.addEventListener("click", eventoBotondom3);
-function eventoBotondom3() {
-    console.log(arregloDeAlumnos)
-} */
 
 let botondom4 = document.createElement("button");
-botondom4.innerHTML="cargar notas guardadas desde local storage";
-document.body.append(botondom4);
+botondom4.innerHTML="cargar notas desde local storage";
+botones.append(botondom4);
 
 botondom4.addEventListener("click", eventoBotondom4);
 function eventoBotondom4() {
     obtenerNotasGuardadas();
     renderizarAlumnos();
+
+    Toastify({
+        text: "Notas cargadas desde local storage",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom",
+        position: "left", 
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to right, #ffeb7f , #a3cd3f)",
+        },
+        onClick: function(){} 
+    }).showToast();
 }
 
 let botondom5 = document.createElement("button");
 botondom5.innerHTML="clear";
-document.body.append(botondom5);
+botones.append(botondom5);
 
 botondom5.addEventListener("click", eventoBotondom5);
 function eventoBotondom5() {
@@ -239,4 +165,117 @@ function eventoBotondom5() {
     
     renderizarAlumnos();
 
+
+    Toastify({
+        text: "Información de local storage borrada",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", 
+        position: "left", 
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to right, #c27ba0 , #660000)",
+        },
+        onClick: function(){} 
+    }).showToast();
 }
+
+//al recargar la página y hacer click en cualquier carrera, se solicita información de la misma proveniente del archivo JSON
+
+function renderizarCarreras(){
+    Toastify({
+        text: "Información de carreras solicitada",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", 
+        position: "left", 
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to left, #00008b , #3aa4f3)",
+        },
+        onClick: function(){} 
+    }).showToast();
+
+
+    tablacarreras.innerHTML="";
+    let tabladom2 = document.getElementById("tablacarreras")
+    tablacarreras.innerHTML=`
+    <table>
+        <thead>
+            <tr>
+                <th>Carrera</th>
+                <th>Cantidad de Materias</th>
+                <th>Duración</th>
+                <th>Descripción</th>
+            </tr>
+        </thead>
+        <tbody id="data">
+        
+        </tbody>
+    </table>`;
+    
+    
+    fetch("./carreras.json")
+    .then(function(Response){
+        return Response.json();
+    })
+    .then(function(carreras){
+        let placeholder = document.querySelector("#data")
+        let salida = "";
+        for(let carrera1 of carreras){
+            salida +=`
+            <tr>
+                <td>${carrera1.carrera}</td>
+                <td>${carrera1.cantidad_de_materias}</td>
+                <td>${carrera1.duracion}</td>
+                <td>${carrera1.descripcion}</td>
+            </tr>
+            `;
+        }
+        placeholder.innerHTML=salida;
+    })
+
+    let botonfinal
+if(!document.getElementById("botoncerrar")){
+botonfinal=document.createElement("button");
+botonfinal.id="botoncerrar";
+botonfinal.innerHTML="Cerrar";
+document.getElementById("tablacarreras").appendChild(botonfinal)
+}
+botonfinal.addEventListener("click", cerrar);
+function cerrar() {
+    const variablecarreras = document.getElementById("tablacarreras")
+    const padre = variablecarreras.parentNode;
+    padre.removeChild(variablecarreras);
+    
+
+    Toastify({
+        text: "Se cerró la información de las carreras",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom",
+        position: "left", 
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to right, #00008b , #3aa4f3)",
+        },
+        onClick: function(){} 
+    }).showToast();
+
+    }
+
+} 
+
+
+document.querySelectorAll('.clickcarrera').forEach(elemento => {
+    elemento.addEventListener('click', renderizarCarreras);
+});
+
+
+
